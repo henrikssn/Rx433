ea1235dabf54adaceb7693584134d38c6e355591
Erik Henriksson <erikhenrikssn@gmail.com>
2020-04-10 12:58:05 +0200

    Add documentation to Rx433.h

--- Rx433.h
+++ Rx433.h
@@ -1,35 +1,56 @@
 #ifndef Rx433_h
 #define Rx433_h
 
+// TODO: Make this compile on AVR platforms (this is ESP8266/ESP32 only
+//       for now).
 #include <Arduino.h>
 #include <list>
 #include <vector>
 
-#define RX_PIN 4
-
-#define DEBUG_PRINT_PULSES
+// Add this to your sketch for the library to write unhandled packets
+// to Serial port. Useful for developing new subclasses of Handler.
+//#define DEBUG_PRINT_PULSES
 
 static int last_sync_us = 0;
 
 namespace rx433 {
 
 struct Pulse {
+  // time_us is the result of microseconds() at the time the packet
+  // was recieved.
+  // delta_us is the difference to the previous time_us at the time
+  // the packet was recieved.
   uint32_t time_us, delta_us;
+  // state is true iff the pin was high.
   bool state;
 };
 
+// Handler should be implemented by a class that supports a specific
+// modulation scheme.
 class Handler {
  public:
+  // IsSync takes a single Pulse and returns true if this is possibly
+  // a valid sync pulse for this handler. Try to make this as specific
+  // as possible as it improves performance of the interrupt handler.
   virtual bool IsSync(const Pulse& p) = 0;
+  // Handle takes a vector of pulses and demodulates them, returning
+  // true if successful. If this returns false, the library will give
+  // the pulse vector to the next handler.
   virtual bool Handle(const std::vector<Pulse>& buf) = 0;
 };
 
+// AddHandler adds a new handler to the supported list of handlers
+// and should be called before Setup.
 void AddHandler(Handler*);
+// Setup should be called once in Arduino setup() function.
 void Setup(int rxPin);
+// Loop should be called in Arduino loop() function.
 void Loop();
 
 } // namespace rx433
 
+// rx433_internal contains implementation specifics and should
+// not be relied upon. We will break you without notice.
 namespace rx433_internal {
 
 using namespace rx433;
